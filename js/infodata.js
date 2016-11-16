875d7d2f72d959160bd532e739765a3a70627657
yuhsun123 <gq5517@gmail.com>
2016-11-16 16:40:42 +0800

    2
    傷眼抱歉

---
+++ js/infodata.js
@@ -0,0 +1,18 @@
+
+
+var jsonString = '{ \
+	"images":\
+	[\
+		{"key" : "scene1","fileName" : "401C.jpg"},\
+		{"key" : "scene2","fileName" : "402C.jpg"},\
+		{"key" : "scene3","fileName" : "407C.jpg"},\
+		{"key" : "scene4","fileName" : "4061.jpg"},\
+		{"key" : "scene5","fileName" : "4062.jpg"},\
+		{"key" : "scene6","fileName" : "4071.jpg"},\
+		{"key" : "scene7","fileName" : "4072.jpg"},\
+		{"key" : "scene8","fileName" : "A402_1.jpg"},\
+		{"key" : "scene9","fileName" : "402_2.jpg"},\
+		{"key" : "scene10","fileName" : "mark.jpg"},\
+		{"key" : "scene11","fileName" : "HW.jpg"}\
+	]\
+}';
